89111f09555ce494866002367c6b7c48bd72dc84
DeltaSpace4 <151398000+DeltaSpace4@users.noreply.github.com>
2024-06-20 21:27:42 +0000

    Teste 1.2

--- Web/Java.js
+++ Web/Java.js
@@ -1,5 +1,7 @@
 let counter = 0;
+let counter2 = 0;
 let nomesLS = [];
+let emailLS = [];
 
 function AddUsuario{
     let usuario = {
@@ -9,12 +11,39 @@ function AddUsuario{
     adicionarNome(usuario);
 }
 
+function AddEmail{
+    let email = {
+        email: emailUsuario.value,
+        id: IDGenerator2(),
+    }
+    adicionarEmail(email);
+}
+
 function adicionarNome(usuario) {
     nomesLS.push(usuario);
     localStorage.setItem('Nome',JSON.stringify(nomesLS));
 }
 
+function adicionarEmail(email) {
+    emailLS.push(email);
+    localStorage.setItem('email',JSON.stringify(emailLS));
+}
+
 function IDGenerator(){
     counter++;
     return counter;
+}
+
+function IDGenerator2(){
+    counter2++;
+    return counter2;
+}
+
+function LimparLS(){
+    localStorage.clear;
+}
+
+function Remover(){
+    emailLS.splice(index, 1);
+    localStorage.setItem('email', JSON.stringify(emailLS));
 }
