9998eccb01bfcd1730674d4e8a1cc4ee31d43777
DeltaSpace4 <151398000+DeltaSpace4@users.noreply.github.com>
2024-06-21 18:04:31 +0000

    Teste 1.7

--- Web/Java.js
+++ Web/Java.js
@@ -2,55 +2,12 @@ let counter = 0;
 let counter2 = 0;
 let nomesLS = [];
 let emailLS = [];
+const lista = document.getElementById("LCadastro");
+const email = document.getElementById("emailADM");
+const nome = document.getElementById("nomeADM");
 
-function AddUsuario{
-    let usuario = {
-        nome: nomeUsuario.value,
-        id: IDGenerator(),
-    }
-    adicionarNome(usuario);
-}
-
-function AddEmail{
-    let email = {
-        email: emailUsuario.value,
-        id: IDGenerator2(),
-    }
-    adicionarEmail(email);
-}
-
-function adicionarNome(usuario) {
-    nomesLS.push(usuario);
-    localStorage.setItem('Nome',JSON.stringify(nomesLS));
-}
-
-function adicionarEmail(email) {
-    emailLS.push(email);
-    localStorage.setItem('email',JSON.stringify(emailLS));
-}
-
-function IDGenerator(){
-    counter++;
-    return counter;
-}
-
-function IDGenerator2(){
-    counter2++;
-    return counter2;
-}
-
-function LimparLS(){
-    localStorage.clear;
-}
-
-function Remover(){
-    emailLS.splice(index, 1);
-    localStorage.setItem('email', JSON.stringify(emailLS));
-}
-
-function NomeL(){
-    var ul = document.getElementById("emailADM").value;
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("Teste 2"));
-    ul.appendChild(li);
+function addLista(){
+    let li = document.createElement("li");
+    li.innerText=nome.value;
+    lista.appendChild(li);
 }
